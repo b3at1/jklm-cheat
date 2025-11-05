cf3ac715b93c5ea5f51d04cecfde3ba1078afcf4
b3at1 <94728927+b3at1@users.noreply.github.com>
2025-11-05 00:31:08 -0600

    Refactor mouse click handling and word loading

--- jklmcheat.py
+++ jklmcheat.py
@@ -1,33 +1,47 @@
 # jklm.cheat
+# run with: chromium-browser -ozone-platform=x11
 import pyautogui
 from pynput import mouse
 
-with open("words1.txt", "r") as words:
-    word_arr = [word.strip() for word in sorted(words)]
+# load word list
+with open("jlawer.txt", "r") as words:
+    word_arr = sorted(w.strip() for w in words if w.strip())
 
 used_words = set()
-typing_active = [False]  # flag to start typing
-stop_flag = [False]      # flag to stop everything
 
+# flags (using lists so they’re mutable from inside listener)
+typing_active = [False]
+stop_flag = [False]
+
+# mouse event handler
 def on_click(x, y, button, pressed):
-    if pressed:
-        if button == mouse.Button.left:
-            typing_active[0] = True  # start typing
-        if button == mouse.Button.right:
-            stop_flag[0] = True      # stop everything
-            return False
+    if not pressed:
+        return
+    if button == mouse.Button.left:
+        typing_active[0] = True           # start typing words
+    elif button == mouse.Button.right:
+        stop_flag[0] = True               # stop generating for current substring
+        typing_active[0] = False          # reset typing state
 
-# start a global listener
+# start the listener (don’t ever stop it)
 listener = mouse.Listener(on_click=on_click)
 listener.start()
 
 while True:
-    substr = input("substring> ")
+    substr = input("substring> ").strip()
+    if not substr:
+        continue
+
+    # reset flags for this round
+    stop_flag[0] = False
+    typing_active[0] = False
+
     for word in word_arr:
         if stop_flag[0]:
             break
+
         if substr in word and word not in used_words and word.isalpha():
-            # wait until left click has been pressed
+            # wait until left-click pressed
             while not typing_active[0]:
                 if stop_flag[0]:
                     break
@@ -37,6 +51,3 @@ while True:
             pyautogui.write(word, interval=0.001)
             pyautogui.press("enter")
             used_words.add(word)
-
-
-listener.stop()
